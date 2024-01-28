50e15cc868b975dc2f5bcbe1cca61e99364ef5cc
dmitry.b <dmitry.b@zala.com.au>
2024-01-28 20:08:42 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:5173'], // Add your frontend URL(s) here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   await app
     .listen(3002)
     .then(() => console.log('successfully started on port 3002'))
